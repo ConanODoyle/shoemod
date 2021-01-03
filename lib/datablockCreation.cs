058de725a28633e22dce62c00f1791ff9333b272
ConanODoyle <samuel.w.sze@gmail.com>
2021-01-03 00:11:26 -0800

    Add support scripts

---
+++ lib/datablockCreation.cs
@@ -0,0 +1,41 @@
+function createItemDatablock(%datablockName, %shapeFileDir)
+{
+	if (isObject(%datablockName))
+	{
+		return %datablockName.getID();
+	}
+
+	%evalString = "" SPC 
+	"datablock ItemData(" @ %datablockName @ ")" SPC
+	"{" SPC
+		"shapeFile = \"" @ %shapeFileDir @ "\";" SPC
+		"mass = 1;" SPC
+		"density = 0.2;" SPC
+		"elasticity = 0.2;" SPC
+		"friction = 0.6;" SPC
+		"emap = 1;" SPC
+		"canDrop = 1;" SPC
+	"};";
+
+	eval(%evalString);
+	return %datablockName.getID();
+}
+
+function createPlayerDatablock(%datablockName, %shapeFileDir)
+{
+	if (isObject(%datablockName))
+	{
+		return %datablockName.getID();
+	}
+
+	%evalString = "" SPC 
+	"datablock PlayerData(" @ %datablockName @ ")" SPC
+	"{" SPC
+		"emap = 0;" SPC
+		"className = \"Armor\";" SPC
+		"shapeFile = \"" @ %shapeFileDir @ "\";" SPC
+	"};";
+
+	eval(%evalString);
+	return %datablockName.getID();
+}
